31ed96342b0a68b03fb40f8373ed93690652105d
ss-stewart <ss_stewart92@yahoo.com>
2023-06-30 17:44:24 -0400

    edit bike schema

--- models/bikeSchema.js
+++ models/bikeSchema.js
@@ -1,11 +1,13 @@
 const Schema = require('mongoose').Schema;
 
 const bikeSchema = new Schema({
-  name: { type: String, required: true },
-  emoji: String,
-  make: {type: Schema.Types.ObjectId, ref: 'Make'},
-  a: { type: Number, required: true }
-}, {
+  make: {type: String, required: true },
+  model: {type: Schema.Types.ObjectId, ref: 'Make'},
+  type: {type: String, required: true },
+  picture: {type: INT, required: true },
+  year: {type: Number, required: true }
+  engineSize: {type: Number, required: true }
+  averageCost: { type: Number, required: true }
   timestamps: true
 });
 
